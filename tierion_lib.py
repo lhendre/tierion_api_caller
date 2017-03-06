40b87bb1d21937384a5bef80e1543c4b56a34310
Lucas Hendren <lucashendren@Lucass-MacBook-Pro.local>
2017-03-05 22:47:39 -0800

    cleaned code

--- tierion_lib.py
+++ tierion_lib.py
@@ -9,16 +9,14 @@ class TierionHash:
 
     def __init__(self, name ,password):
         self.hashAuthEndpoint='https://hashapi.tierion.com/v1/auth/token'
-        self.password=password
-        self.nameUser=name
-        self.HashAuth=requests.post(self.hashAuthEndpoint,data = {'username':self.nameUser,'password':self.password})
+        self.HashAuth=requests.post(self.hashAuthEndpoint,data = {'username':name,'password':password})
 
     def refresh(self):
         data=self.HashAuth.json()
         token=data['refresh_token']
         self.HashAuth=requests.post('https://hashapi.tierion.com/v1/auth/refresh', data={'refreshToken':token})
 
-    def submit_hash_item(self, item):
+    def submitHashItem(self, item):
         try:
             hashedItem= hashlib.sha256(item.encode('utf-8')).hexdigest()
             data=self.HashAuth.json()
@@ -28,11 +26,11 @@ class TierionHash:
             receipt=requests.post('https://hashapi.tierion.com/v1/hashitems', headers=headers, data=payload)
             return receipt.json()['receiptId']
         except Exception as e:
-            print('No connection. Got an error code: '+repr(e))
+            print('No connection. Error code: '+repr(e))
             raise e
 
 
-    def get_receipt(self, id_receipt):
+    def getReceipt(self, id_receipt):
         try:
             data=self.HashAuth.json()
             token=data['access_token']
@@ -43,7 +41,7 @@ class TierionHash:
         except Exception as e:
             print('No connection. Got an error code: '+repr(e))
             raise e
-    def get_all_blocksubscritption(self):
+    def getAllBlockSubscriptions(self):
         try:
             data=self.HashAuth.json()
             token=data['access_token']
@@ -53,7 +51,7 @@ class TierionHash:
             print('No connection. Got an error code: '+repr(e))
             raise e
 
-    def get_block_subscription(self, id):
+    def getBlockSubscription(self, id):
         try:
             data=self.HashAuth.json()
             token=data['access_token']
@@ -63,7 +61,7 @@ class TierionHash:
             print('No connection. Got an error code: '+repr(e))
             raise e
 
-    def create_callBack_subscriptioin(self,callback,newLabel):
+    def createBlockSubscription(self,callback,newLabel):
         try:
             data=self.HashAuth.json()
             token=data['access_token']
@@ -73,7 +71,7 @@ class TierionHash:
             print('No connection. Got an error code: '+repr(e))
             raise e
 
-    def update_block_subscription(self,id,callback,newLabel):
+    def updateBlockSubscription(self,id,callback,newLabel):
         try:
             data=self.HashAuth.json()
             token=data['access_token']
@@ -83,7 +81,7 @@ class TierionHash:
             print('No connection. Got an error code: '+repr(e))
             raise e
 
-    def delete_block_subscription(self,id,callback,newLabel):
+    def deleteBlockSubscription(self,id,callback,newLabel):
         try:
             data=self.HashAuth.json()
             token=data['access_token']
@@ -92,16 +90,3 @@ class TierionHash:
         except Exception as e:
             print('No connection. Got an error code: '+repr(e))
             raise e
-
-def validate_tierion_receipt(us_original_file, receipt_dict):
-    url = 'https://blockchain.info/rawtx/{}'.format(receipt_dict['anchors'][0]['sourceId'])
-    try:
-        trx_dict = requests.get(url, headers={'content-type': 'application/json'}).json()
-    except:
-        return (False, False, False)
-    # 1: check that the merkle root exists as the OP_RETURN value of the ouput[0] script
-    b_in_blockchain = '6a20'+receipt_dict['merkleRoot'] == trx_dict['out'][0]['script']
-    mt = MerkleTools()
-    b_proof_valid = mt.validate_proof(receipt_dict['proof'], receipt_dict['targetHash'], receipt_dict['merkleRoot'])
-    b_hashes_match = hashlib.sha256(us_original_file.encode('utf-8')).hexdigest() == receipt_dict['targetHash']
-    return b_in_blockchain, b_proof_valid, b_hashes_match
